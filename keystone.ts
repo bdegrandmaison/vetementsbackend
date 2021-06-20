8efdf3a9c939a7bbb480a49cdcca39e962f3b6c4
B. de Grandmaison <bdegrandmaison@hotmail.fr>
2021-06-20 22:39:42 +0200

    heroku cookies

--- keystone.ts
+++ keystone.ts
@@ -22,6 +22,7 @@ const databaseURL = process.env.DATABASE_URL || "mongodb://localhost/vetements";
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
   secret: process.env.COOKIE_SECRET,
+  secure: false
 };
 
 const { withAuth } = createAuth({
