05372dbf6517b53deab563fb63e99fe051a1dab2
B. de Grandmaison <bdegrandmaison@hotmail.fr>
2021-06-21 07:15:31 +0200

    Clearance

--- keystone.ts
+++ keystone.ts
@@ -44,7 +44,7 @@ export default withAuth(
     // @ts-ignore
     server: {
       cors: {
-        origin: [process.env.FRONTEND_URL , process.env.FRONTEND_URL2 ],
+        origin: [process.env.FRONTEND_URL  ],
         credentials: true,
       },
     },
